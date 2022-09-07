a69dd82f08c76d6d86a044741b160f258b50daf5
gambore <rey_christian_gamboa@manulife.ca>
2022-09-08 00:03:46 +0800

    4. Handling Form Data - added payment feature

--- src/app.js
+++ src/app.js
@@ -23,7 +23,6 @@ app.get('/checking', (req, res) => res.render('account', { account: accounts.che
 app.get('/credit', (req, res) => res.render('account', { account: accounts.credit }));
 
 app.get('/transfer', (req, res) => res.render('transfer'));
-
 app.post('/transfer', (req, res) => {
   accounts[req.body.from].balance -= req.body.amount;
   accounts[req.body.to].balance += parseInt(req.body.amount, 10);
@@ -31,6 +30,16 @@ app.post('/transfer', (req, res) => {
   fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf-8');
   res.render('transfer', {message: 'Transfer Completed.'});
 });
+
+app.get('/payment', (req, res) => res.render('payment', { account: accounts.credit }));
+app.post('/payment', (req, res) => {
+  accounts.credit.balance -= req.body.amount;
+  accounts.credit.available += parseInt(req.body.amount);
+  let accountsJSON = JSON.stringify(accounts, null, 4);
+  fs.writeFileSync(path.join(__dirname, 'json','accounts.json'), accountsJSON, 'utf-8');
+  res.render('payment', { message: 'Payment Successful', account: accounts.credit });
+})
+
 
 app.get('/profile', (req, res) => res.render('profile', { user: users[0] }));
 
