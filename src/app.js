40dbd34fc29f177557b7fed784b800455994d02c
gambore <rey_christian_gamboa@manulife.ca>
2022-09-07 23:56:10 +0800

    4. Handling Form Data - convert account data to JSON

--- src/app.js
+++ src/app.js
@@ -27,6 +27,7 @@ app.get('/transfer', (req, res) => res.render('transfer'));
 app.post('/transfer', (req, res) => {
   accounts[req.body.from].balance -= req.body.amount;
   accounts[req.body.to].balance += parseInt(req.body.amount, 10);
+  let accountsJSON = JSON.stringify(accounts, null, 4)
 });
 
 app.get('/profile', (req, res) => res.render('profile', { user: users[0] }));
