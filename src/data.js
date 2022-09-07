a7760258cdaf0a3abd2043c8b60b1e167fd6a3c6
gambore <rey_christian_gamboa@manulife.ca>
2022-09-08 00:22:23 +0800

    5. Creating a Data Access Library - write JSON function body

--- src/data.js
+++ src/data.js
@@ -6,3 +6,8 @@ const accounts = JSON.parse(accountData);
 
 const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf-8');
 const users = JSON.parse(userData);
+
+const writeJSON = () => {
+  let accountsJSON = JSON.stringify(accounts, null, 4);
+  fs.writeFileSync(path.join(__dirname, 'json','accounts.json'), accountsJSON, 'utf8');
+}
