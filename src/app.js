d194eaefc2025d1608cf6eada7cbec333117ed63
gambore <rey_christian_gamboa@manulife.ca>
2022-09-08 00:46:40 +0800

    6. Using the Express Router - require services and account routes

--- src/app.js
+++ src/app.js
@@ -6,6 +6,9 @@ const app = new express();
 
 const { accounts, users, writeJSON } = require('./data.js');
 
+const accountRoutes = require('./routes/accounts.js');
+const servicesRoutes = require('./routes/services.js');
+
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
@@ -13,8 +16,6 @@ app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts: accounts }));
-
-
 
 app.get('/profile', (req, res) => res.render('profile', { user: users[0] }));
 
