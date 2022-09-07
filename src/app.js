c528745d24a1811ad473bf88d785fad0f5d9f5eb
gambore <rey_christian_gamboa@manulife.ca>
2022-09-07 22:13:26 +0800

    2. Use Express to Create a Server - create the index route

--- src/app.js
+++ src/app.js
@@ -8,3 +8,5 @@ app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/', (req, res) => res.render('index', { title: 'Index' }));
