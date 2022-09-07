1820f9607e785e3250e6a21f77bb033cc83f053a
gambore <rey_christian_gamboa@manulife.ca>
2022-09-07 22:50:21 +0800

    fix

--- src/app.js
+++ src/app.js
@@ -18,8 +18,8 @@ const users = JSON.parse(userData);
 app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts: accounts }));
 
 app.get('/savings', (req, res) => res.render('account', { account: accounts.savings }));
-app.get('/checking', (req, res) => res.render('account', { checking: accounts.checking }));
-app.get('/credit', (req, res) => res.render('account', { credit: accounts.credit }));
+app.get('/checking', (req, res) => res.render('account', { account: accounts.checking }));
+app.get('/credit', (req, res) => res.render('account', { account: accounts.credit }));
 
 app.get('/profile', (req, res) => res.render('profile', { user: users[0] }));
 
