fcd0f27f31a2b98bab0e3e92eeb6d7a5dd54cef4
gambore <rey_christian_gamboa@manulife.ca>
2022-09-08 00:41:56 +0800

    6. Using the Express Router - export the router

--- src/routes/accounts.js
+++ src/routes/accounts.js
@@ -5,3 +5,5 @@ const { accounts } = require('../data.js');
 router.get('/savings', (req, res) => res.render('account', { account: accounts.savings }));
 router.get('/checking', (req, res) => res.render('account', { account: accounts.checking }));
 router.get('/credit', (req, res) => res.render('account', { account: accounts.credit }));
+
+module.exports = router;
