d355456cfa0b7d80d58d6a38d253afbf4474f3c7
gambore <rey_christian_gamboa@manulife.ca>
2022-09-08 00:20:26 +0800

    5. Creating a Data Access Library - transistion user and account data library

--- src/data.js
+++ src/data.js
@@ -1,2 +1,8 @@
 const fs = require('fs');
 const path = require('path');
+
+const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf-8');
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf-8');
+const users = JSON.parse(userData);
