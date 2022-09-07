9fef4abe433164e2e78bfd33c726c1ecb5278c27
gambore <rey_christian_gamboa@manulife.ca>
2022-09-07 22:46:03 +0800

    3. File Handling and Routing - create the profile route

--- src/app.js
+++ src/app.js
@@ -21,4 +21,6 @@ app.get('/savings', (req, res) => res.render('account', { account: accounts.savi
 app.get('/checking', (req, res) => res.render('checking', { checking: accounts.checking }));
 app.get('/credit', (req, res) => res.render('credit', { credit: accounts.credit }));
 
+app.get('/profile', (req, res) => res.render('profile', { user: users[0] }));
+
 app.listen(3000, () => { console.log('PS Project Running on port 3000!') });
